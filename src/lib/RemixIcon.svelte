9518cb37fa36de6bbb19dd00d88d7cf3b119e5bc
pavelBelyaev98 <pavelMilkman@yandex.ru>
2021-12-11 19:59:46 +0200

    first commit

---
+++ src/lib/RemixIcon.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	export let src;
+	export let size = '100%';
+	export let filled = false;
+	let customClass = '';
+	export { customClass as class };
+	export let ariaHidden = false;
+</script>
+
+<svg
+	class={customClass}
+	aria-hidden={ariaHidden}
+	width={size}
+	fill="currentColor"
+	height={size}
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+>
+	<g>
+		{#if src.length > 1}
+			{#if filled}
+				{#each src[0] as att}
+					<path {...att} />
+				{/each}
+			{:else}
+				{#each src[1] as att}
+					<path {...att} />
+				{/each}
+			{/if}
+		{:else}
+			{#each src[0] as att}
+				<path {...att} />
+			{/each}
+		{/if}
+	</g>
+</svg>
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import RemixIcon from 'svelte-remix-icons/RemixIcon.svelte';
+	import { AncientGate } from 'svelte-remix-icons';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<RemixIcon src={AncientGate} />
